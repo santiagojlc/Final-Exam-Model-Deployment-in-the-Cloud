318655e41a255b187886a1cdb7ce17ee8818c1a2
santiagojlc <157887590+santiagojlc@users.noreply.github.com>
2024-05-18 23:18:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,29 @@ def load_data():
     })
     return temperature_data
 
-st.title("Detroit Daily Temperatures with Artificial Warming")
+st.title("Daily Temperatures with Artificial Warming")
 
 # Adding navigation and other sections
 st.sidebar.title("Navigation")
 navigation = st.sidebar.radio("Go to", ["Home", "Explore", "About"])
 
 if navigation == "Home":
-    st.write("Welcome to our page! Here, we delve into the fascinating world of Detroit's Daily Temperatures with Artificial Warming. Our focus lies in predicting and forecasting the temperature trends over the upcoming two months. Moreover, we keenly observe how global warming impacts these trends, offering insights into the evolving climate scenario.")
-    st.write("Join us as we analyze, predict, and visualize the temperature trends, empowering you with valuable insights into the future climate of this vibrant city.")
+    st.markdown("""
+    <div style='text-align: justify;'>
+    Welcome to our page! Here, we delve into the fascinating world of Detroit's Daily Temperatures with Artificial Warming. Our focus lies in predicting and forecasting the temperature trends over the upcoming two months. Moreover, we keenly observe how global warming impacts these trends, offering insights into the evolving climate scenario.
+    </div>
+    """, unsafe_allow_html=True)
+    st.markdown("""
+    <div style='text-align: justify;'>
+    Join us as we analyze, predict, and visualize the temperature trends, empowering you with valuable insights into the future climate of this vibrant city.
+    </div>
+    """, unsafe_allow_html=True)
 elif navigation == "Explore":
     st.subheader("Daily Temperatures with Artificial Warming")
     st.image("gets.jfif", caption="This graph presents the predictions and forecasts of daily temperatures in Detroit from 2012 to 2017 and beyond.", use_column_width=True)
 elif navigation == "About":
-    st.write("Through this platform, you'll embark on a journey through time and temperature, exploring the intricate interplay between artificial influences and natural climatic patterns. As we unravel the data, you'll gain a deeper understanding of the dynamic nature of Detroit's temperature landscape.")
+    st.markdown("""
+    <div style='text-align: justify;'>
+    Through this platform, you'll embark on a journey through time and temperature, exploring the intricate interplay between artificial influences and natural climatic patterns. As we unravel the data, you'll gain a deeper understanding of the dynamic nature of Detroit's temperature landscape.
+    </div>
+    """, unsafe_allow_html=True)
