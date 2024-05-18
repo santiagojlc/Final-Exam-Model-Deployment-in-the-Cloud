66e207bc290a1ee0e3acc001269d9e17ca25bfdb
santiagojlc <157887590+santiagojlc@users.noreply.github.com>
2024-05-18 21:20:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,11 @@ import streamlit as st
 import numpy as np
 import tensorflow as tf
 
-@st.cache
+@st.cache_resource
 def load_model(filepath):
     model = tf.keras.models.load_model(filepath)
     return model
 
-def generate_forecast(model, year):
-    # Generate forecast for the given year using the model
-    # Replace this with your actual code to generate the forecast
-    forecast = np.random.randn(10)  # Dummy forecast for demonstration
-    return forecast
-
 st.title("Daily Temperatures with Artificial Warming")
 
-file_path = r'C:\Users\HP\Desktop\STREAMLIT\my_arima_model.h5'
-model = load_model(file_path)
-
-# Display forecast for each year
-for year in range(2022, 2030):  # Adjust the range based on your requirement
-    st.header(f"Forecast for Year {year}")
-    forecast = generate_forecast(model, year)
-    st.write(f"Forecast for Year {year}: {forecast}")
+model = load_model('C:\Users\HP\Desktop\STREAMLIT\my_arima_model.h5')
