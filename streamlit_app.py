ae0f5e0c6c90514430b5cbc092a768a2517b2020
santiagojlc <157887590+santiagojlc@users.noreply.github.com>
2024-05-19 11:17:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import os
 
 # Dynamically determine the path to the ARIMA model file
-model_path = Path(__file__).resolve().parent.parent / 'pages' / 'ARIMA_model.h5'
+model_path = Path(__file__).resolve().parent.parent / 'pages' / 'arima_model.pkl'
 
 # Load ARIMA model with error handling
 @st.cache_resource
