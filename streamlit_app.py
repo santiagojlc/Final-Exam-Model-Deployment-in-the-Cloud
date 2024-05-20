9cb7fccb6e48a8dfd879972e5a61300d9a37c44e
santiagojlc <157887590+santiagojlc@users.noreply.github.com>
2024-05-20 12:15:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,9 @@ st.sidebar.title("Navigation")
 navigation = st.sidebar.radio("Go to", ["Home", "Explore", "About"])
 
 if navigation == "Home":
-    st.write("Welcome to the Home section.")
+    st.write("Welcome to our page! Here, we delve into the fascinating world of Detroit's Daily Temperatures with Artificial Warming. Our focus lies in predicting and forecasting the temperature trends over the upcoming two months. Moreover, we keenly observe how global warming impacts these trends, offering insights into the evolving climate scenario.")
 elif navigation == "Explore":
     st.subheader("Graph showing daily temperatures can be viewed below")
     st.image("Graph.jfif", caption="Daily Temperatures with Artificial Warming", use_column_width=True)
 elif navigation == "About":
-    st.write("About this app.")
+    st.write("Through this platform, you'll embark on a journey through time and temperature, exploring the intricate interplay between artificial influences and natural climatic patterns. As we unravel the data, you'll gain a deeper understanding of the dynamic nature of Detroit's temperature landscape. Join us as we analyze, predict, and visualize the temperature trends, empowering you with valuable insights into the future climate of this vibrant city.")
